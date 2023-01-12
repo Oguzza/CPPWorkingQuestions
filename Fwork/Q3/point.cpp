fd1c1992c1883b261a2dccea6c4ca0b650346b03
Oguzza <84422343+Oguzza@users.noreply.github.com>
2023-01-12 08:25:33 +0300

    Update point.cpp

--- Fwork/Q3/point.cpp
+++ Fwork/Q3/point.cpp
@@ -100,3 +100,84 @@ bool zGT5(Point &a)
     else
         return false;
 }
+
+-------------------------------------------------------------------------------------
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <iomanip>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+using namespace std;
+
+struct Point{
+	Point(double a = 0, double b = 0, double c = 0){
+		z = a;
+		y = b;
+		x = c;
+	}
+	double x;
+	double y;
+	double z;
+};
+
+
+
+Point initPoint(void)
+{
+	Point dummy(rand()%100 /(double)10, rand()%100 /(double)10, rand()%100 /(double)10 );
+	return dummy;
+}
+
+void printPoint(vector< Point > a)
+{
+	cout << "  X\tY\tZ" << endl;
+	for(vector<Point>::iterator it = a.begin(); it != a.end(); it++)
+	{
+		cout << setfill(' ') << setw(3) << it->x <<  "\t" << it->y << "\t" << it->z << endl;
+	}
+}
+
+bool zGT5(Point a)
+{
+	if(a.z > 5)return true;
+	else return false;
+}
+
+typedef map<int, Point > MiP;
+
+int main(int argc, char** argv) {
+	vector <Point> points(20);	
+	MiP map;
+
+	generate(points.begin(), points.end(), initPoint);
+	cout << "Point Vector: " << endl;
+	printPoint(points);
+	
+	vector <int> mask(20);
+	
+	transform(points.begin(), points.end(), mask.begin(), zGT5);
+	
+	cout << "\nMask Vector:" << endl;
+	for(int i = 0; i < mask.size(); i++)
+	{
+		cout << mask[i] << " ";
+		if(mask[i] == 1) 
+		{
+			map.insert(make_pair(i,points[i]));
+		}
+	}
+	
+	cout << "\n\nPoint Map; " << endl;
+	cout << "Key\tX\tY\tZ" << endl;
+	for(MiP::iterator it = map.begin(); it != map.end(); it++)
+	{
+		cout << setfill(' ') << setw(3) << it->first << "\t" << it->second.x << "\t" << it->second.y << "\t" << it->second.z << endl;
+	}
+	
+	
+	
+	return 0;
+}
